545bd5d03d582cbb69f40aa9aca54c8aa260abcf
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2022-01-07 03:05:07 -0600

    API

---
+++ API/Controllers/UiowaController.cs
@@ -0,0 +1,48 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UiowaController : ControllerBase
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult SaveReimbursementData([FromForm] Reimbursement reimbursement)
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var savePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Images");
+
+                if(file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(savePath, fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { reimbursement.Date, reimbursement.Description, reimbursement.Amount ,fullPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Intenal server error {ex}");
+            }
+
+        }
+    }
+}
---
+++ API/Models/Reimbursement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Reimbursement
+    {
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+        public IFormFile Receipt { get; set; }
+    }
+}
